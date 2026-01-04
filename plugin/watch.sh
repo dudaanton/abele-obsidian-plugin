bb7fef28a46e3562c6d0f2347ad016f350dfabe9
Anton Duda <anton@duda.dev>
2026-01-05 00:54:34 +0300

    feat: improved startup script for plugin development

--- plugin/watch.sh
+++ plugin/watch.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-npx nodemon --exec "./build_and_copy_plugin_prod.sh" --watch src --ext js,ts,vue
+# MODE=${1:-prod}
+MODE=${1}
+
+case $MODE in
+  prod)
+    SCRIPT="./build_and_copy_plugin_prod.sh"
+    ;;
+  test)
+    SCRIPT="./build_and_copy_plugin_test.sh"
+    ;;
+  *)
+    echo "Usage: $0 [prod|test]"
+    echo "  prod - production build (default)"
+    echo "  test - test build"
+    exit 1
+    ;;
+esac
+
+npx nodemon --exec "$SCRIPT" --watch src --ext js,ts,vue
